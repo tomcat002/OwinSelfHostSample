7f083727e06e21612350505ce25f7473d3ac48d7
Eric Jing <jyg002@sina.com>
2017-05-20 09:59:31 +1000

    Add project files.

---
+++ OwinSelfHostSample/Program.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OwinSelfHostSample
+{
+    class Program
+    {
+        public class Startup
+        {
+            // This code configures Web API. The Startup class is specified as a type
+            // parameter in the WebApp.Start method.
+            public void Configuration(IAppBuilder appBuilder)
+            {
+                // Configure Web API for self-host. 
+                HttpConfiguration config = new HttpConfiguration();
+                config.Routes.MapHttpRoute(
+                    name: "DefaultApi",
+                    routeTemplate: "api/{controller}/{id}",
+                    defaults: new { id = RouteParameter.Optional }
+                );
+
+                appBuilder.UseWebApi(config);
+            }
+        }
+
+        public class ValuesController : ApiController
+        {
+            // GET api/values 
+            public IEnumerable<string> Get()
+            {
+                return new string[] { "value1", "value2" };
+            }
+
+            // GET api/values/5 
+            public string Get(int id)
+            {
+                return "value";
+            }
+
+            // POST api/values 
+            public void Post([FromBody]string value)
+            {
+            }
+
+            // PUT api/values/5 
+            public void Put(int id, [FromBody]string value)
+            {
+            }
+
+            // DELETE api/values/5 
+            public void Delete(int id)
+            {
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:9000/";
+
+            // Start OWIN host 
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient();
+
+                var response = client.GetAsync(baseAddress + "api/values").Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
