cff6a35c21efdd0446d782d8313efcf1f7f9e17d
Eric Jing <jyg002@sina.com>
2017-05-20 16:43:49 +1000

    fix bug.

--- OwinSelfHostSample/Program.cs
+++ OwinSelfHostSample/Program.cs
@@ -9,53 +9,6 @@ namespace OwinSelfHostSample
 {
     class Program
     {
-        public class Startup
-        {
-            // This code configures Web API. The Startup class is specified as a type
-            // parameter in the WebApp.Start method.
-            public void Configuration(IAppBuilder appBuilder)
-            {
-                // Configure Web API for self-host. 
-                HttpConfiguration config = new HttpConfiguration();
-                config.Routes.MapHttpRoute(
-                    name: "DefaultApi",
-                    routeTemplate: "api/{controller}/{id}",
-                    defaults: new { id = RouteParameter.Optional }
-                );
-
-                appBuilder.UseWebApi(config);
-            }
-        }
-
-        public class ValuesController : ApiController
-        {
-            // GET api/values 
-            public IEnumerable<string> Get()
-            {
-                return new string[] { "value1", "value2" };
-            }
-
-            // GET api/values/5 
-            public string Get(int id)
-            {
-                return "value";
-            }
-
-            // POST api/values 
-            public void Post([FromBody]string value)
-            {
-            }
-
-            // PUT api/values/5 
-            public void Put(int id, [FromBody]string value)
-            {
-            }
-
-            // DELETE api/values/5 
-            public void Delete(int id)
-            {
-            }
-        }
 
         static void Main(string[] args)
         {
@@ -73,6 +26,54 @@ namespace OwinSelfHostSample
                 Console.WriteLine(response.Content.ReadAsStringAsync().Result);
                 Console.ReadLine();
             }
+        }
+    }
+
+    public class ValuesController : ApiController
+    {
+        // GET api/values 
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5 
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values 
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5 
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5 
+        public void Delete(int id)
+        {
+        }
+    }
+
+    public class Startup
+    {
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            appBuilder.UseWebApi(config);
         }
     }
 }
